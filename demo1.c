f684198a37e49b720d4ae696b7bc2456c90b9660
Palak222 <80598844+Palak222@users.noreply.github.com>
2021-09-10 23:44:46 +0530

    Update demo1.c

--- demo1.c
+++ demo1.c
@@ -8,19 +8,6 @@ typedef struct{
 
 }fraction;
 
-int compareNum(void *p1, void *p2)
-{
-        int *a = (int*)p1;
-        int *b = (int*)p2;
-
-        if((*a) > (*b))
-		return 1;
-	else if((*a) < (*b))
-		return -1;
-	else
-		return 0;
-}
-
 int compareFrac(void *a, void *b)
 {
 	fraction *f1 = (fraction*)a;
@@ -39,15 +26,10 @@ int main()
 {
 	fraction array[5] = { {1, 16}, {1, 8}, {3, 8}, {5, 6}, {31, 32} };
 	fraction ele = {6, 16};
+	
+	int result = binarysearch(array, sizeof(fraction), 5, &ele, compareFrac);
 
-        int arr[7] = {10, 30, 50, 46, 60, 78, 90};
-	int key = 90;
-
-        int result1 = binarysearch(arr, sizeof(int), 7, &key, compareNum);
-	int result2 = binarysearch(array, sizeof(fraction), 5, &ele, compareFrac);
-
-	printf("%d ", result1);
-	printf("%d ", result2);
+	printf("%d ", result);
 
         return 0;
 }
